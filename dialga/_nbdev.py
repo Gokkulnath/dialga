217d0ae3308f81f49e021108312ea9e6da6561be
Gokkulnath T S <gokkulnath@gmail.com>
2020-02-26 14:06:52 +0530

    Initial Drop

--- dialga/_nbdev.py
+++ dialga/_nbdev.py
@@ -2,9 +2,40 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"Resize_Image": "01_datasetops.ipynb",
+         "Resize_Imgs_Directory": "01_datasetops.ipynb",
+         "stratified_sample_df": "01_datasetops.ipynb",
+         "CyclicLR": "02_keras.callbacks.ipynb",
+         "get_data": "02_keras.datasets.ipynb",
+         "model_totpu": "02_keras.datasets.ipynb",
+         "dice_coef": "02_keras.losses.ipynb",
+         "dice_loss": "02_keras.losses.ipynb",
+         "bce_dice_loss": "02_keras.losses.ipynb",
+         "bce_logdice_loss": "02_keras.losses.ipynb",
+         "weighted_bce_loss": "02_keras.losses.ipynb",
+         "weighted_dice_loss": "02_keras.losses.ipynb",
+         "weighted_bce_dice_loss": "02_keras.losses.ipynb",
+         "lovasz_grad": "02_keras.losses.ipynb",
+         "lovasz_hinge": "02_keras.losses.ipynb",
+         "lovasz_hinge_flat": "02_keras.losses.ipynb",
+         "flatten_binary_scores": "02_keras.losses.ipynb",
+         "lovasz_softmax": "02_keras.losses.ipynb",
+         "lovasz_softmax_flat": "02_keras.losses.ipynb",
+         "flatten_probas": "02_keras.losses.ipynb",
+         "keras_lovasz_softmax": "02_keras.losses.ipynb",
+         "keras_lovasz_hinge": "02_keras.losses.ipynb",
+         "focal_loss": "02_keras.losses.ipynb",
+         "mean_iou": "02_keras.metrics.ipynb",
+         "FilteredDataset": "03a_torch.datasets.ipynb",
+         "accuracy": "03b_torch.metrics.ipynb"}
 
-modules = []
+modules = ["datasetops.py",
+           "keras/callbacks.py",
+           "keras/datasets.py",
+           "keras/losses.py",
+           "keras/losses.py",
+           "torch/datasets.py",
+           "torch/metrics.py"]
 
 doc_url = "https://gokkulnath.github.io/dialga/"
 
